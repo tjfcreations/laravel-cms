7036b68f1e40c5ceda610a341a32a2338d54af99
Tjalling Feenstra <hello@tjalling.net>
2025-10-12 19:01:21 +0200

    chore: rename Locale::get() to Locale::findByCode()

--- src/I18n/Models/Locale.php
+++ src/I18n/Models/Locale.php
@@ -10,27 +10,45 @@ class Locale extends Model {
 
     protected $guarded = [];
 
+    /**
+     * Get all locales except the default locale (memoized).
+     */
     public static function allNotDefault($columns = ['*']) {
         return once(fn() => parent::where('is_default', false)->get($columns));
     }
 
+    /**
+     * Get all machine translatable locales (memoized).
+     */
     public static function allMachineTranslatable($columns = ['*']): Collection {
         return once(fn() => parent::where('is_machine_translatable', true)->get($columns));
     }
 
+    /**
+     * Get all locales with the default locale last (memoized).
+     */
     public static function allWithDefaultLast($columns = ['*']) {
         return once(fn() => parent::all($columns)->sortBy('is_default'));
     }
 
+    /**
+     * Get all locales with the default locale first (memoized).
+     */
     public static function allWithDefaultFirst($columns = ['*']) {
         return once(fn() => parent::all($columns)->sortByDesc('is_default'));
     }
 
+    /**
+     * Get the default locale (memoized).
+     */
     public static function getDefault(): self {
         return once(fn() => static::where('is_default', true)->first());
     }
 
-    public static function get(string $code): ?self {
+    /**
+     * Find a locale by its code (memoized).
+     */
+    public static function findByCode(string $code): ?self {
         return once(fn() => static::where('code', $code)->first());
     }
 
