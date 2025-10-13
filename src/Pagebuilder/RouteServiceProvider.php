350956a93bcd8e8789fb73340a4e3076db3c392b
Tjalling Feenstra <hello@tjalling.net>
2025-10-13 14:51:34 +0200

    fix: only allow hreflangs to be specific patterns

--- src/Pagebuilder/RouteServiceProvider.php
+++ src/Pagebuilder/RouteServiceProvider.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
 use Feenstra\CMS\Pagebuilder\Http\Controllers\PageController;
 use Feenstra\CMS\Pagebuilder\Middleware\SetLocale;
+use Feenstra\CMS\I18n\Models\Locale;
 
 class RouteServiceProvider extends ServiceProvider {
     public function boot(): void {
@@ -26,18 +27,21 @@ class RouteServiceProvider extends ServiceProvider {
 
         Route::middleware('web')
             ->group(function () {
-                foreach (Page::all() as $page) {
-                    if ($page->type === PageTypeEnum::Error) {
-                    }
+                $hreflangs = Locale::pluck('hreflang')->toArray();
+                $hreflangPattern = implode('|', $hreflangs);
 
+                foreach (Page::all() as $page) {
                     foreach (Page::all() as $page) {
                         // register a normal get-route
                         Route::get($page->path, [PageController::class, 'show'])
                             ->defaults('pageId', $page->id);
 
                         // register a localized get-route
-                        Route::get('{locale}/' . ltrim($page->path, '/'), [PageController::class, 'show'])
-                            ->defaults('pageId', $page->id);
+                        if (!empty($hreflangs)) {
+                            Route::get('{hreflang}/' . ltrim($page->path, '/'), [PageController::class, 'show'])
+                                ->defaults('pageId', $page->id)
+                                ->where('hreflang', $hreflangPattern);
+                        }
                     }
                 }
             });
