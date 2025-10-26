698e1709e4b3b7e84ed7c09b8e059dfad3b15836
Tjalling Feenstra <hello@tjalling.net>
2025-10-26 14:15:28 +0100

    chore: recache routes on locale edit

--- src/I18n/Filament/Resources/LocaleResource/Pages/CreateLocale.php
+++ src/I18n/Filament/Resources/LocaleResource/Pages/CreateLocale.php
@@ -6,18 +6,20 @@ use Feenstra\CMS\I18n\Filament\Resources\LocaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
+use Feenstra\CMS\Pagebuilder\Jobs\RecacheRoutes;
 
-class CreateLocale extends CreateRecord
-{
+class CreateLocale extends CreateRecord {
     protected static string $resource = LocaleResource::class;
 
-    protected function handleRecordCreation(array $data): Model
-    {
+    protected function handleRecordCreation(array $data): Model {
         $record = parent::handleRecordCreation($data);
 
-        if($record->is_default) {
+        if ($record->is_default) {
             $record->setAsDefault();
         }
+
+        // recache because of new hreflang
+        RecacheRoutes::dispatchAfterResponse();
 
         return $record;
     }
--- src/I18n/Filament/Resources/LocaleResource/Pages/EditLocale.php
+++ src/I18n/Filament/Resources/LocaleResource/Pages/EditLocale.php
@@ -6,25 +6,26 @@ use Feenstra\CMS\I18n\Filament\Resources\LocaleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
+use Feenstra\CMS\Pagebuilder\Jobs\RecacheRoutes;
 
-class EditLocale extends EditRecord
-{
+class EditLocale extends EditRecord {
     protected static string $resource = LocaleResource::class;
 
-    protected function getHeaderActions(): array
-    {
+    protected function getHeaderActions(): array {
         return [
             Actions\DeleteAction::make(),
         ];
     }
 
-    protected function handleRecordUpdate(Model $record, array $data): Model
-    {
+    protected function handleRecordUpdate(Model $record, array $data): Model {
         $record = parent::handleRecordUpdate($record, $data);
 
-        if($record->is_default) {
+        if ($record->is_default) {
             $record->setAsDefault();
         }
+
+        // hreflang may have changed
+        RecacheRoutes::dispatchAfterResponse();
 
         return $record;
     }
