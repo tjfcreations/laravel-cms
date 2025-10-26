e086c1264a982ba9bdacce4decdc35527952260e
Tjalling Feenstra <hello@tjalling.net>
2025-10-26 16:55:04 +0100

    chore: implement logviewer permissions

--- src/Common/CommonServiceProvider.php
+++ src/Common/CommonServiceProvider.php
@@ -4,11 +4,23 @@ namespace Feenstra\CMS\Common;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Feenstra\CMS\Common\Filament\Pages\LogViewer;
 
 class CommonServiceProvider extends ServiceProvider {
     public function boot(): void {
+        $this->setupLoginRoute();
+        $this->setupLogViewerAuth();
+    }
+
+    protected function setupLoginRoute(): void {
         if (!Route::has('login')) {
             Route::redirect('/login', '/admin/login/')->name('login');
         }
+    }
+
+    protected function setupLogViewerAuth(): void {
+        LogViewer::auth(function ($request) {
+            return $request->user() && $request->user()->canAccessPanel();
+        });
     }
 }
