5fef545e51a58700f0e171721f0a0695e4496c33
Tjalling Feenstra <hello@tjalling.net>
2025-10-26 14:12:21 +0100

    chore: remove default page type

--- src/Pagebuilder/Enums/PageTypeEnum.php
+++ src/Pagebuilder/Enums/PageTypeEnum.php
@@ -5,14 +5,12 @@ namespace Feenstra\CMS\Pagebuilder\Enums;
 use Filament\Support\Contracts\HasLabel;
 
 enum PageTypeEnum: string implements HasLabel {
-    case Default = 'default';
     case Generic = 'generic';
     case Template = 'template';
     case Error = 'error';
 
     public function getLabel(): string {
         return match ($this) {
-            self::Default => 'default',
             self::Template => 'Template',
             self::Generic => 'Standaardpagina',
             self::Error => 'Foutpagina'
